10418f6c577ee244f51830a6ea0c118d265e54d0
xenoroxy <renasoong@gmail.com>
2024-04-29 14:37:33 +0800

    move prac 2 to 3

--- app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
@@ -1,6 +1,8 @@
 package sg.edu.np.mad.madpractical3;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        User user = new User("Rena Soong", "MAD Developer", 1, false);
+
+        TextView tvName = findViewById(R.id.tvname);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+        Button btnFollow = findViewById(R.id.btnFollow);
+
+        tvName.setText(user.name);
+        tvDescription.setText(user.description);
+        btnFollow.setText("Follow");
     }
 }
